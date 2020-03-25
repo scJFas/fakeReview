761045eeee0138d2b6f56583461d0bc5acc4c145
scJFas <43513989+scJFas@users.noreply.github.com>
2020-03-25 19:43:55 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,22 @@
+
+import jieba
+import gensim
+import pandas as pd
+
+MODEL_PATH = 'baike_26g_news_13g_novel_229g.model'
+
+OUTPUT_FILE = 'index2vector.csv'
+
+def main():
+    model = gensim.models.Word2Vec.load(MODEL_PATH)
+    print('词向量维度：', model.wv.vectors.shape)
+    #print(model.wv.vectors)
+
+    index2vector = pd.DataFrame(columns=['index'])
+    index2vector['index'] = model.wv.index2word
+
+    index2vector.to_csv(OUTPUT_FILE, header=False, index=False)
+
+
+if __name__ == "__main__":
+    main()
