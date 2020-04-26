ef44459283e4a5fe33ad731ed77fbe8a5c2c4387
oushu1huangben1 <huang.ben@oushu.io>
2020-04-26 13:33:58 +0800

    update data_clear

--- preProcess/data_clear.py
+++ preProcess/data_clear.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
 
-INPUT_FILE = "jieba_chinese_10w.csv"
+INPUT_FILE = "keep_only_chinese.csv"
 
-OUTPUT_FILE = "jieba_chinese_10w.csv"
+OUTPUT_FILE = "cleared.csv"
 
 LOGREASON = ["5","6","16","17","18","-1"]
 
@@ -15,10 +15,44 @@ def clear_notNum(meta_data):
             meta_data = meta_data.drop([i])
     return meta_data.reset_index(drop=True)
 
+def clear_all_dataTypes(meta_data):
+
+    if isinstance(meta_data['reviewid'][0], np.int64) == False:
+        for i in range(len(meta_data)):
+            if str.isdigit(meta_data['reviewid'][i]) == False:
+                meta_data.drop([i], inplace=True)
+    meta_data.reset_index(drop=True, inplace=True)
+
+    if isinstance(meta_data['logreason'][0], np.int64) == False:
+        for i in range(len(meta_data)):
+            if meta_data['logreason'][i] not in LOGREASON:
+                meta_data.drop([i], inplace=True)
+    meta_data.reset_index(drop=True, inplace=True)
+
+    if isinstance(meta_data['updatetime'][0], np.int64) == False:
+        for i in range(len(meta_data)):
+            try:
+                pd.to_datetime(meta_data['updatetime'][i], format='%Y/%m/%d %H:%M')
+            except ValueError:
+                meta_data.drop([i], inplace=True)
+            except :
+                print("pd.to_datetime unknown error")
+                return []
+    meta_data.reset_index(drop=True, inplace=True)
+
+    # if isinstance(meta_data['userid'][0], np.int64) == False:
+    # if isinstance(meta_data['shopid'][0], np.int64) == False:
+    # if isinstance(meta_data['star'][0], np.int64) == False:
+    # if isinstance(meta_data['score1'][0], np.int64) == False:
+    # if isinstance(meta_data['score2'][0], np.int64) == False:
+    # if isinstance(meta_data['score3'][0], np.int64) == False:
+
+    return meta_data
+
 
 def main():
     meta_data = pd.read_csv(INPUT_FILE, header=0)
-    meta_data = clear_notNum(meta_data)
+    meta_data = clear_all_dataTypes(meta_data)
     meta_data.to_csv(OUTPUT_FILE, header=True, index= False)
 
 if __name__ == "__main__":
